212674e230b92f05b01ab2b4c97ccecfd2b2bb15
Emanuel E. Sosa <emanuelesausosa@gmail.com>
2020-10-21 19:06:46 -0600

    add more strings function

--- D1S2/string.js
+++ D1S2/string.js
@@ -8,6 +8,8 @@
 //  [1,5,5,5,4,4,]
 //
 
+const _ = require("lodash");
+
 let soyUnString = "Hola mundo";
 let soyUnNumero = 4;
 // for in (1)
@@ -31,13 +33,63 @@ Array.of(soyUnString).forEach((char) => {
 
 // for in
 for (let i in soyUnString) {
-    console.log(soyUnString[i]);
+  // console.log(soyUnString[i]);
 }
 
 // for of
 for (let i of soyUnString) {
-  console.log(i);
+  // console.log(i);
 }
 
 // while
+let index = 0;
+while (index < soyUnString.length) {
+  console.log(soyUnString[index]);
+  index++;
+}
 
+// console.log(_.concat([1], 2, 3, [3]));
+
+// funciones con strings
+// split
+const fecha = "25/02/2016";
+const arrayFecha = fecha.split("/");
+console.log(arrayFecha);
+
+const regexPattern = /[0-9]+|\+|-|\*|/gm;
+const suma = "52+52-65*41";
+// const numerosASumar = suma.split(regexPattern);
+const numerosASumar = suma.match(regexPattern);
+console.log("numeros a sumar", numerosASumar);
+// regex  -> Regular Expression
+
+const listaDeCorreos = "abc@mm.com, abc@mm.com,abc@mm.com,abc@mm.com";
+const correos = listaDeCorreos.split(",");
+console.log(correos);
+
+// includes
+const oracion = "Padre nuestro que estas en los cielos, que";
+const include = oracion.includes("a"); // return boolenan true | false
+console.log(include);
+// match
+const match = oracion.match("que"); // return an array
+console.log(match);
+
+// substring -> extraer caracteres (index inicial, index final)
+const foundSubString = oracion.substring(5, 10);
+console.log(foundSubString);
+
+// substr -> extraer caracteres (index inicial, numero de caracters hacia delante)
+const foundSubStr = oracion.substr(5, 10);
+console.log(foundSubStr);
+// toLowerCase
+console.log(oracion.toLocaleLowerCase());
+
+// toUpperCase
+console.log(oracion.toUpperCase());
+
+oracion.replace(""); // (new word, old word)
+
+// ejercicio
+// poner mayuscula solo la primera letra de cada palabra
+// 
