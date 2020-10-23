1a5eda8704f641078bd3abc0d5f3bb0d1c2e49eb
Emanuel E. Sosa <emanuelesausosa@gmail.com>
2020-10-22 18:59:29 -0600

    add array d

---
+++ S2/D4/arrays.js
@@ -0,0 +1,8 @@
+let miArray = [1, 5, 8, 8, 5, 4];
+// let miArray2 = new Array().push(5);
+
+console.log(miArray);
+// console.log(miArray2);
+
+let miArray2 = ["hola", 4, true, { key: "soy la key" }, [1, 1, 47, 5]];
+console.log(miArray2);
