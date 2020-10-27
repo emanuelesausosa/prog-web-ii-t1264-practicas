138318c7ae685cf14c6ecc56cef4cf128e252a08
Emanuel Sosa <emanuelesausosa@gmail.com>
2020-10-26 19:20:04 -0600

    add types

---
+++ S3/D1-types.ts
@@ -0,0 +1,23 @@
+// tipos de datos
+//              any -> dinamico, 
+// |                                |    
+// definidos por el lenguaje  ---  Defeinidos por el usuario (dev)
+// [string, number, boolean, [], void,]      [Array, Clases, interfaces, types, enum]
+
+var soyUnAny: any = [];
+var soyUnAny2: any = "";
+var soyUnAny3: any;
+
+// definidos por el lenguaje
+// string
+var soyString: string = "Hola";
+
+// number
+var soyNumber: number = 66;
+
+// boolean
+var soyBoolean: boolean = true;
+
+// arreglo tipado
+var soyArrayNumbers: number[] = [1,1,4,4,7];
+
---
+++ S3/D1.ts
@@ -0,0 +1,26 @@
+// Typescript
+// 1. que es? -> por que?
+    // libreria ?? 
+    // framework ?? -> tipos, java
+    // lenguaje -> js -> superset, superpoderes, tipos var, let, const  = "", 1, true,
+    // Programacion Orientada a Objetos, -> C# JIT, Java, C++
+    // Verbose -> hace mucho codigo
+    // extends (herencia), implements (contratos, interfaces)
+    
+ // por que?
+ // seguridad (errores no manejados), errores en tiempo de desarrollo
+ // -> reducir tiempo debbuging
+ // -> POO -
+ // react - 
+ // vue -> 
+ // odia a Angular
+ // webpack -> 
+
+ // 
+
+// 2. instalacion
+// tsc
+// 3. <<hello world>>
+const saludo = "Hola!";
+console.log(saludo);
+// Typos
